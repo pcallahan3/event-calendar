03f34c2b5801f115e272cbf6224353ede6e19f02
Patrick <pcallahan3@mail.greenriver.edu>
2017-08-30 18:20:22 -0700

    Created Categories and Events views

---
+++ src/AppBundle/Controller/EventController.php
@@ -0,0 +1,54 @@
+<?php
+
+/* EventController for event actions
+*/
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class EventController extends Controller
+{
+    /**
+     * @Route("/events", name="event_list")
+     */
+    public function indexAction(Request $request)
+    {
+        // Render index.html.twig from category directory - app/Resources/views/category
+        return $this->render('events/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/event/create", name="event_create")
+     */
+    public function createAction(Request $request)
+    {
+        // Render create.html.twig from category directory - app/Resources/views/category
+        return $this->render('events/create.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+     /**
+     * @Route("/event/edit/{id}", name="event_edit")
+     */
+    public function editAction(Request $request)
+    {
+        // Render edit.html.twig from category directory - app/Resources/views/category
+        return $this->render('events/edit.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/event/delete/{id}", name="event_delete")
+     */
+    public function deleteAction(Request $request)
+    {
+         // No view to render
+    }
+}
