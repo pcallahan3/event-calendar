56e214ecb75a0e9aae747dd97dda3bcd1579fdea
Patrick <pcallahan3@mail.greenriver.edu>
2017-09-02 18:14:38 -0700

    Added create functionailty to the CategoryController

--- src/AppBundle/Controller/CategoryController.php
+++ src/AppBundle/Controller/CategoryController.php
@@ -9,6 +9,11 @@ use AppBundle\Entity\Category; //Include the entity for database/model parsing
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\DateType;
+
+
 
 class CategoryController extends Controller
 {
@@ -30,9 +35,26 @@ class CategoryController extends Controller
      */
     public function createAction(Request $request)
     {
-        // Render create.html.twig from category directory - app/Resources/views/category
+        //Create new Category object
+        $category = new Category;
+
+        //Pass in Category object so a form can be built with the current fields 
+        $form = $this->createFormBuilder($category)->add('name', TextType::class, array('attr' => array('class' => 'form-control', 
+                'style' => 'margin-bottom:15px')))->add('save', SubmitType::class, array('label' => 'Create Category', 'attr' =>
+                 array('class' => 'btn btn-primary')))->getForm();
+
+        //Handle request
+        $form->handleRequest($request);
+
+        //Check to see if data is submitted
+        if($form->isSubmitted() && $form->isValid()){
+            $name = $form['name']->getData();
+            die($name);
+        }
+
+        // Render create.html.twig template from category directory - app/Resources/views/category
         return $this->render('category/create.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'form' => $form->createView()
         ]);
     }
 
